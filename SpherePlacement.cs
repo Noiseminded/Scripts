e33e8ef7a0e6670b48a65608dc63cbf8ac4af0f2
Noiseminded <abelneto@live.com>
2016-09-16 05:13:33 +0100

    Added work already done

---
+++ SpherePlacement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpherePlacement : MonoBehaviour {
+
+	public GameObject spherePrefab;
+
+	public float bounds = 200;
+
+	public AudioClip[] clips;
+
+	void Awake()
+	{
+		
+
+		for (int i = 0; i < 16; i++) {
+
+			Vector3 randomLocation = new Vector3 (Random.Range (-bounds, bounds), Random.Range (-bounds, bounds), Random.Range (-bounds, bounds));
+
+			GameObject sphere = Instantiate (spherePrefab, randomLocation, transform.rotation) as GameObject;
+
+			sphere.GetComponent<AudioSource> ().clip = clips [i];
+			sphere.GetComponent<AudioSource> ().Play(0);
+
+
+		}
+
+	}
+}
